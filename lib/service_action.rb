61e48deaf9e224aa145b0c392891ec4272836d9e
Kali Donovan <kali@teamshares.com>
2025-02-20 08:48:19 -0800

    Initial skeleton

---
+++ lib/service_action.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "service_action/version"
+
+module ServiceAction
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/service_action/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ServiceAction
+  VERSION = "0.1.0"
+end
