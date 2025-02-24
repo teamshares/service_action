824b8c78fec7bcab46b78eb56496d8f02a681f36
Kali Donovan <kali@teamshares.com>
2025-02-24 10:11:39 -0800

    Improve organizer support

--- lib/service_action/organizer.rb
+++ lib/service_action/organizer.rb
@@ -1,13 +1,26 @@
 # frozen_string_literal: true
 
 module ServiceAction
+  # NOTE: replaces, rather than layers on, the upstream Interactor::Organizer module (only three methods, and
+  # we want ability to implement a more complex interface where we pass options into the organized interactors)
   module Organizer
     def self.included(base)
       base.class_eval do
-        include Interactor::Organizer
         include ::ServiceAction
 
+        extend ClassMethods
         include InstanceMethods
+      end
+    end
+
+    # NOTE: pulled unchanged from https://github.com/collectiveidea/interactor/blob/master/lib/interactor/organizer.rb
+    module ClassMethods
+      def organize(*interactors)
+        @organized = interactors.flatten
+      end
+
+      def organized
+        @organized ||= []
       end
     end
 
