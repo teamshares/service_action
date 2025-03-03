18b37cfe23e1f5a247a27b5c687c354af1159cb2
Kali Donovan <kali@teamshares.com>
2025-03-03 10:04:30 -0800

    depends_on improvements

--- lib/service_action/depends_on.rb
+++ lib/service_action/depends_on.rb
@@ -9,15 +9,23 @@ module ServiceAction
     end
 
     module InstanceMethods
+      private
+
       # TODO: extract to own layer (and - maybe don't take direct call, decompose instead?)
       # TODO: downstream uses the wrapped version, and I think we want that to set thread-local variables anyway
       def depends_on(error_prefix: nil)
         raise ArgumentError, "must provide a block to execute" unless block_given?
 
         result = yield @context
+        # TODO: ensure this has spec coverage
+        raise "#depends_on block must return a service call (expected to implement ok?)" unless result.respond_to?(:ok?)
         return if result.ok?
 
-        fail_with([error_prefix, result.error].compact.join(" "))
+        handle_depended_upon_output(result, error_prefix:)
+      end
+
+      def handle_depended_upon_output(result, error_prefix: nil)
+        fail_with([error_prefix, result.error].compact.join(": "))
       end
     end
   end
