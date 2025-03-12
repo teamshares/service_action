aaef9ceca7f257201bf15cf429923d787fe3f2e2
Kali Donovan <kali@teamshares.com>
2025-03-12 09:51:05 -0700

    Extract ContractValidator

---
+++ lib/action/contract_validator.rb
@@ -0,0 +1,63 @@
+module Action
+  class ContractValidator
+    include ActiveModel::Validations
+
+    def initialize(context)
+      @context = context
+    end
+
+    def read_attribute_for_validation(attr)
+      @context.public_send(attr)
+    end
+
+    def self.validate!(validations:, direction:, context:)
+      validator = Class.new(Action::ContractValidator) do
+        def self.name = "Action::ContractValidator::OneOff"
+
+        validations.each do |field, field_validations|
+          field_validations.each do |key, value|
+            validates field, key => value
+          end
+        end
+      end.new(context)
+
+      return if validator.valid?
+
+      exception_klass = direction == :inbound ? Action::Contract::InboundViolation : Action::Contract::OutboundViolation
+      raise exception_klass, validator.errors
+    end
+
+    # Allow for custom validators to be defined in the context of the interactor
+    class ValidateValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        msg = begin
+          options[:with].call(value)
+        rescue StandardError => e
+          # TODO: log this error?
+          "failed validation: #{e.message}"
+        end
+
+        record.errors.add(attribute, msg) if msg.present?
+      end
+    end
+
+    class BooleanValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        return if [true, false].include?(value)
+
+        record.errors.add(attribute, "must be true or false")
+      end
+    end
+
+    class TypeValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        return if value.blank? # Handled with a separate default presence validator
+
+        types = options[:in] || Array(options[:with])
+
+        msg = types.size == 1 ? "is not a #{types.first}" : "is not one of #{types.join(", ")}"
+        record.errors.add attribute, (options[:message] || msg) unless types.any? { |type| value.is_a?(type) }
+      end
+    end
+  end
+end
--- lib/action/restrict_context_access.rb
+++ lib/action/restrict_context_access.rb
@@ -4,6 +4,8 @@ require "active_model"
 require "active_support/core_ext/enumerable"
 require "active_support/core_ext/module/delegation"
 
+require "action/contract_validator"
+
 module Action
   module RestrictContextAccess
     def self.included(base)
@@ -146,23 +148,10 @@ module Action
       def validate_context!(direction)
         raise ArgumentError, "Invalid direction: #{direction}" unless %i[inbound outbound].include?(direction)
 
-        directional_validations = self.class.instance_variable_get("@#{direction}_validations")
-        directional_context = direction == :inbound ? inbound_context : outbound_context
+        validations = self.class.instance_variable_get("@#{direction}_validations")
+        context = direction == :inbound ? inbound_context : outbound_context
 
-        validator = Class.new(ContextValidator) do
-          def self.name = "OneoffContextValidator"
-
-          directional_validations.each do |field, field_validations|
-            field_validations.each do |key, value|
-              validates field, key => value
-            end
-          end
-        end.new(directional_context)
-
-        return if validator.valid?
-
-        exception_klass = direction == :inbound ? Action::Contract::InboundViolation : Action::Contract::OutboundViolation
-        raise exception_klass, validator.errors
+        ContractValidator.validate!(validations:, direction:, context:)
       end
 
       def context_for_logging(direction = nil)
@@ -184,52 +173,6 @@ module Action
 
       def sensitive_fields
         self.class.instance_variable_get("@sensitive_fields").compact
-      end
-    end
-
-    # TODO: this does not appear to be thread safe -- clearing validations whenever setting -_-
-    class ContextValidator
-      include ActiveModel::Validations
-
-      def initialize(context)
-        @context = context
-      end
-
-      def read_attribute_for_validation(attr)
-        @context.public_send(attr)
-      end
-
-      # Allow for custom validators to be defined in the context of the interactor
-      class ValidateValidator < ActiveModel::EachValidator
-        def validate_each(record, attribute, value)
-          msg = begin
-            options[:with].call(value)
-          rescue StandardError => e
-            # TODO: log this error?
-            "failed validation: #{e.message}"
-          end
-
-          record.errors.add(attribute, msg) if msg.present?
-        end
-      end
-
-      class BooleanValidator < ActiveModel::EachValidator
-        def validate_each(record, attribute, value)
-          return if [true, false].include?(value)
-
-          record.errors.add(attribute, "must be true or false")
-        end
-      end
-
-      class TypeValidator < ActiveModel::EachValidator
-        def validate_each(record, attribute, value)
-          return if value.blank? # Handled with a separate default presence validator
-
-          types = options[:in] || Array(options[:with])
-
-          msg = types.size == 1 ? "is not a #{types.first}" : "is not one of #{types.join(", ")}"
-          record.errors.add attribute, (options[:message] || msg) unless types.any? { |type| value.is_a?(type) }
-        end
       end
     end
 
